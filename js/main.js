c846acf6d7417b604fba44b16d63b74ac7dd7769
isupov_andrey <isupov_andrey@icloud.com>
2020-04-08 18:40:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,203 @@
+var clock = {
+	"init":"<?xml version='1.0' encoding='utf-8'?> <svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 1136 380' xml:space='preserve'>",
+	"background":"<rect fill='#000000' width='1136' height='380'/>",
+	"initend":"</svg>",
+
+	
+	"seg":{
+		"dig": function (dig,seg){
+				
+				switch(dig) {
+				case 0: {
+				switch(seg) {
+					case "a" : return "<polygon points='949,66 1060.9,66 1088.8,40 1081,31 934.9,31 925.6,40'/>"; break;
+					case "b" : return "<polygon points='1062.1,67.1 1053.1,169.9 1068.7,187 1087.3,169.9 1097.7,50 1089.9,41.5'/>"; break;
+					case "c" : return "<polygon points='1049.9,208.4 1040.9,311.2 1064.3,336.8 1073.5,328.2 1084,208.4 1068.4,191.3'/>"; break;
+					case "d" : return "<polygon points='1039.5,313 927.6,313 899.7,338.5 907.5,347 1053.6,347 1062.9,338.5'/>"; break;
+					case "e" : return "<polygon points='926.4,311.2 935.4,208.4 919.8,191.3 901.3,208.4 890.8,328.2 898.6,336.8'/>"; break;	
+					case "f" : return "<polygon points='938.7,169.9 947.6,67.1 924.3,41.5 915,50 904.5,169.9 920.1,187'/>"; break;	
+					case "g" : return "<polygon points='1051.7,173 939.8,173 921.2,189.5 936.8,206 1048.7,206 1067.3,189.5'/>"; break;	
+					case "dp": return "<circle cx='1103.9' cy='329.7' r='17'/>"; break;
+					default: "";
+				}
+					
+				}break;
+				
+				case 1: {
+				switch(seg) {
+					case "a" : return "<polygon points='665,66 776.9,66 804.8,40 797,31 650.9,31 641.6,40'/>"; break;
+					case "b" : return "<polygon points='778.1,67.1 769.1,169.9 784.7,187 803.3,169.9 813.7,50 805.9,41.5'/>"; break;
+					case "c" : return "<polygon points='765.9,208.4 756.9,311.2 780.3,336.8 789.5,328.2 800,208.4 784.4,191.3'/>"; break;
+					case "d" : return "<polygon points='755.5,313 643.6,313 615.7,338.5 623.5,347 769.6,347 778.9,338.5'/>"; break;
+					case "e" : return "<polygon points='642.4,311.2 651.4,208.4 635.8,191.3 617.3,208.4 606.8,328.2 614.6,336.8'/>"; break;	
+					case "f" : return "<polygon points='654.7,169.9 663.6,67.1 640.3,41.5 631,50 620.5,169.9 636.1,187'/>"; break;	
+					case "g" : return "<polygon points='767.7,173 655.8,173 637.2,189.5 652.8,206 764.7,206 783.3,189.5'/>"; break;	
+					case "dp": return "<circle cx='819.9' cy='329.7' r='17'/>"; break;	
+					default: "";
+				}
+					
+				}break;
+				
+				case 2: {
+				switch(seg) {
+					case "a" : return "<polygon points='381,66 492.9,66 520.8,40 513,31 366.9,31 357.6,40'/>"; break;
+					case "b" : return "<polygon points='494.1,67.1 485.1,169.9 500.7,187 519.3,169.9 529.7,50 521.9,41.5'/>"; break;
+					case "c" : return "<polygon points='481.9,208.4 472.9,311.2 496.3,336.8 505.5,328.2 516,208.4 500.4,191.3'/>"; break;
+					case "d" : return "<polygon points='471.5,313 359.6,313 331.7,338.5 339.5,347 485.6,347 494.9,338.5'/>"; break;
+					case "e" : return "<polygon points='358.4,311.2 367.4,208.4 351.8,191.3 333.3,208.4 322.8,328.2 330.6,336.8'/>"; break;	
+					case "f" : return "<polygon points='370.7,169.9 379.6,67.1 356.3,41.5 347,50 336.5,169.9 352.1,187'/>"; break;	
+					case "g" : return "<polygon points='483.7,173 371.8,173 353.2,189.5 368.8,206 480.7,206 499.3,189.5'/>"; break;	
+					case "dp": return "<circle cx='535.9' cy='329.7' r='17'/>"; break;	
+					default: "";
+				}
+					
+				}break;	
+
+				case 3: {
+				switch(seg) {
+					case "a" : return "<polygon points='97,66 208.9,66 236.8,40 229,31 82.9,31 73.6,40'/>"; break;
+					case "b" : return "<polygon points='210.1,67.1 201.1,169.9 216.7,187 235.3,169.9 245.7,50 237.9,41.5'/>"; break;
+					case "c" : return "<polygon points='197.9,208.4 188.9,311.2 212.3,336.8 221.5,328.2 232,208.4 216.4,191.3'/>"; break;
+					case "d" : return "<polygon points='187.5,313 75.6,313 47.7,338.5 55.5,347 201.6,347 210.9,338.5'/>"; break;
+					case "e" : return "<polygon points='74.4,311.2 83.4,208.4 67.8,191.3 49.3,208.4 38.8,328.2 46.6,336.8'/>"; break;	
+					case "f" : return "<polygon points='86.7,169.9 95.6,67.1 72.3,41.5 63,50 52.5,169.9 68.1,187'/>"; break;	
+					case "g" : return "<polygon points='199.7,173 87.8,173 69.2,189.5 84.8,206 196.7,206 215.3,189.5'/>"; break;	
+					case "dp": return "<circle cx='251.9' cy='329.7' r='17'/>"; break;	
+					default: "";					
+				}
+					
+				}break;				
+				
+				case 4: {
+				switch(seg) {
+					case "a" : return "<circle cx='574.5' cy='115.9' r='17'/>"; break;
+					case "b" : return "<circle cx='562' cy='264' r='17'/>"; break;
+					default: "";
+				
+				}
+					
+				}break;					
+				
+				default: "";
+				
+				}
+				
+				
+			
+			
+			
+		}
+
+		},
+		
+	
+
+};
+
+
+function mass(dig,mass,dot){
+var temp;
+if(mass == "0") mass = 0;
+if(mass == "1") mass = 1;
+if(mass == "2") mass = 2;
+if(mass == "3") mass = 3;
+if(mass == "4") mass = 4;
+if(mass == "5") mass = 5;
+if(mass == "6") mass = 6;
+if(mass == "7") mass = 7;
+if(mass == "8") mass = 8;	
+if(mass == "9") mass = 9;
+
+if(dig == 4){
+	switch(mass) {
+	case 0: temp = ""; break;
+	case 1: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"b"); break;
+	case 2: temp = temp + clock.seg.dig(dig,"a"); break;
+	case 3: temp = temp + clock.seg.dig(dig,"b"); break;
+	
+	  default: "";
+}	
+}else{
+	switch(mass) {
+	case 0: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"b") + clock.seg.dig(dig,"c") + clock.seg.dig(dig,"d") + clock.seg.dig(dig,"e") + clock.seg.dig(dig,"f"); break;
+	case 1: temp = temp + clock.seg.dig(dig,"b") + clock.seg.dig(dig,"c"); break;
+	case 2: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"b") + clock.seg.dig(dig,"d") + clock.seg.dig(dig,"e") + clock.seg.dig(dig,"g"); break;
+	case 3: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"b") + clock.seg.dig(dig,"c") + clock.seg.dig(dig,"d") + clock.seg.dig(dig,"g"); break;
+	case 4: temp = temp + clock.seg.dig(dig,"b") + clock.seg.dig(dig,"c") + clock.seg.dig(dig,"f") + clock.seg.dig(dig,"g"); break;
+	case 5: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"c") + clock.seg.dig(dig,"d") + clock.seg.dig(dig,"f") + clock.seg.dig(dig,"g"); break;
+	case 6: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"c") + clock.seg.dig(dig,"d") + clock.seg.dig(dig,"e") + clock.seg.dig(dig,"f") + clock.seg.dig(dig,"g"); break;
+	case 7: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"b") + clock.seg.dig(dig,"c"); break;
+	case 8: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"b") + clock.seg.dig(dig,"c") + clock.seg.dig(dig,"d") + clock.seg.dig(dig,"e") + clock.seg.dig(dig,"f") + clock.seg.dig(dig,"g"); break;
+	case 9: temp = temp + clock.seg.dig(dig,"a") + clock.seg.dig(dig,"b") + clock.seg.dig(dig,"c") + clock.seg.dig(dig,"d")+  clock.seg.dig(dig,"f") + clock.seg.dig(dig,"g"); break;
+
+	  default: "";
+}
+}
+
+if(dot == 1) temp = temp + clock.seg.dig(dig,"dp");
+
+return temp;
+}
+
+var outclock = {
+	"dig0":"",
+	"dig1":"",
+	"dig2":"",
+	"dig3":"",
+	"dig4":"",
+	
+	"dig0dp":"",
+	"dig1dp":"",
+	"dig2dp":"",
+	"dig3dp":"",
+	"dig4dp":"",
+	
+	"color":"#ffffff",
+	
+	"sleepcolor":"#ffffff",
+	"sleepcolorOpacity":"0.02",
+	
+	"sleepOnDot":"1",
+	
+	
+	
+	
+	"out": function (){
+document.getElementById("clock").innerHTML = clock.init +       "<g fill='"+outclock.sleepcolor+"' opacity='"+outclock.sleepcolorOpacity+"'>"+mass(0,8,outclock.sleepOnDot)+mass(1,8,outclock.sleepOnDot)+mass(2,8,outclock.sleepOnDot)+mass(3,8,outclock.sleepOnDot)+mass(4,outclock.sleepOnDot)+"</g>"       +         "<g fill='"+outclock.color+"'>"+mass(0,outclock.dig0,outclock.dig0dp)+mass(1,outclock.dig1,outclock.dig1dp)+mass(2,outclock.dig2,outclock.dig2dp)+mass(3,outclock.dig3,outclock.dig3dp)+mass(4,outclock.dig4)+"</g>" + clock.initend;
+
+
+
+
+	}
+	
+};
+
+var clockdot = 1;
+function main(){
+//Wed Apr 08 2020 16:44:37 GMT+0300 (Москва, стандартное время)
+
+
+
+outclock.dig0 = Date()[20];
+outclock.dig1 = Date()[19];
+outclock.dig2 = Date()[17];
+outclock.dig3 = Date()[16];
+
+outclock.dig4 = (clockdot = !clockdot);
+
+
+
+//console.log(!clockdot);
+outclock.out();
+}
+
+setInterval(main, 500);
+
+
+
+
+
+
+
+
+
